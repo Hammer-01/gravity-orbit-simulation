f650852f12028e8a11ec1da824a57bd1fd5fb5f4
Hammer-01 <98681433+Hammer-01@users.noreply.github.com>
2022-02-08 09:30:35 +1000

    Update simulator.js

--- simulator.js
+++ simulator.js
@@ -1,10 +1,23 @@
-
+let Body;
+let bodies = [];
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  text('Hello World!', width/2, height/2);
+    createCanvas(windowWidth, windowHeight);
+    text('Hello World!', width/2, height/2);
+    
+    colorMode(HSB);
+    
+    Body = function(mass, x, y, colour) {
+        this.m = mass;
+        this.x = x || width/2;
+        this.y = y || height/2;
+        this.c = colour || color(random(360),1,1);
+    };
+    Body.prototype.draw = function() {
+        
+    };
 }
 
 function draw() {
-  
+    
 }
