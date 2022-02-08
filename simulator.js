0230292c8244196c76855699e2f13934f8564961
Hammer-01 <98681433+Hammer-01@users.noreply.github.com>
2022-02-08 14:14:12 +1000

    Update simulator.js

--- simulator.js
+++ simulator.js
@@ -1,6 +1,6 @@
 let Body;
 let bodies = [];
-let massRatio = 1;
+let massSizeRatio = 1;
 let G = 6.674;
 
 function setup() {
@@ -23,13 +23,13 @@ function setup() {
     };
     Body.prototype.draw = function() {
         fill(this.c);
-        circle(this.x, this.y, this.m/massRatio);
+        circle(this.x, this.y, this.m * massSizeRatio);
     };
     Body.prototype.update = function(bodies) {
         for (body of bodies) {
             if (body !== this) {
                 // F = G * M * m / r^2
-                let force = G * this.m * body.m / sq(dist(this.x, this.y, body.x, body.y));
+                let force = G * this.m * body.m / sq(dist(this.x, this.y, body.x, body.y)) * 4;
                 let angle = atan2(body.y - this.y, body.x - this.x);
                 // Update acc, vel, pos
                 this.ax += force * cos(angle);
