1c7b04e4445687da99bfc63ee4c62ce5d3f376b5
Hammer-01 <98681433+Hammer-01@users.noreply.github.com>
2022-02-09 21:00:45 +1000

    Update simulator.js

--- simulator.js
+++ simulator.js
@@ -1,18 +1,20 @@
 let Body;
 let bodies = [];
-let G = 6.674;
-let massDistRatio = Math.pow(10, 20);
+let showMassValue = false;
+let maxMass = 10000; // temp number, will set to 0 later
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    console.log(width, height);    
-    
+
     noStroke();
     colorMode(HSB);
     textAlign(CENTER, CENTER);
     
-    Body = function(mass, x, y, vx, vy, colour) {
+    Body = function(mass, fixed, x, y, vx, vy, colour) {
         this.m = mass;
+
+        this.fixed = fixed;
+
         this.x = x || width/2;
         this.y = y || height/2;
         
@@ -21,40 +23,54 @@ function setup() {
         this.ax = 0;
         this.ay = 0;
         
-        this.c = colour || color(random(360), 100, 100);
+        // may deprecate manually setting colour
+        this.c = colour || color(180 - Math.log10(mass) * 180 / Math.log10(maxMass), 100, 100);
     };
     Body.prototype.draw = function() {
         fill(this.c);
-        //circle(this.x, this.y, this.m / pow(10,30));
-        circle(this.x, this.y, 100);
-        fill(0);
-        text(this.m, this.x, this.y);
+        circle(this.x, this.y, 10 * Math.log10(this.m));
+        if (showMassValue) {
+            fill(0);
+            text(this.m, this.x, this.y);
+        }
     };
     Body.prototype.update = function(bodies) {
-        for (body of bodies) {
-            if (body !== this) {
-                // F = G * M * m / r^2
-                let force = G * this.m * body.m / sq(dist(this.x, this.y, body.x, body.y) / massDistRatio);
-                let angle = atan2(body.y - this.y, body.x - this.x);
-                // Update acc, vel, pos
-                this.ax += force * cos(angle);
-                this.ay += force * sin(angle);      
-                this.vx += this.ax;
-                this.vy += this.ay;
-                this.x += this.vx;
-                this.y += this.vy;
+        if (!this.fixed) {
+            for (let body of bodies) {
+                if (body !== this) {
+                    this.ax = this.ay = 0;
+                    // F = G * M * m / r^2
+                    let force = body.m / sq(dist(this.x, this.y, body.x, body.y));
+                    let angle = atan2(body.y - this.y, body.x - this.x);
+                    // Update acc and vel. will update pos later when every body has been processed
+                    this.ax += force * cos(angle);
+                    this.ay += force * sin(angle);
+                    this.vx += this.ax;
+                    this.vy += this.ay;
+                    this.x += this.vx;
+                    this.y += this.vy;
+                }
+            }
+            for (let body of bodies) {
+                if (body !== this) {
+                    this.x += this.vx;
+                    this.y += this.vy;
+                }
             }
         }
         this.draw();
     };
+    Body.prototype.updateColour = function(newBodyMass) { // keep or no?
+        // this.c = 
+    };
     
-    bodies.push(new Body(2*pow(10,30)), new Body(6*pow(10,24), 100, 100));
+    bodies.push(new Body(10000, true), new Body(100, false, width/2, height/4, 5)); // temporary until gui option
 }
 
 function draw() {
     background(255);
-    for (body of bodies) {
+    for (let body of bodies) {
         body.update(bodies);
-        console.log(`x: ${body.x}, y: ${body.y}`);
+        console.log(`m: ${body.m}, vx: ${body.vx}, vy: ${body.vy}`);
     }
 }
